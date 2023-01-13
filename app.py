d315129ba0cb1448f14553c79d29b539a38123bd
Harry Cox <hrjcox@outlook.com>
2023-01-13 22:31:23 +0000

    Variables

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    email = request.form['email']
+    return render_template('index.html', variable = email)
