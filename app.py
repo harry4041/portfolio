16d1390caac8157a419025681cb46a2170f63d57
Harry Cox <hrjcox@outlook.com>
2023-01-13 22:57:40 +0000

    some auth

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
 @app.route('/', methods=['POST'])
-def my_form_post():
-    email = request.form['email']
-    return render_template('index.html', variable = email)
+def signup():
+    email = request.form['SUemail']
+    password = request.form['SUpassword']
+
+    return render_template('index.html', email = email, password = password)
+
+
+
+
+
