c14c1ba9f203d5ed8d0cf1c27566f39f6ee90dbd
Harry Cox <hrjcox@outlook.com>
2023-01-14 00:07:13 +0000

    Initial Flask App

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
-
-@app.route('/', methods=['POST'])
-def signup():
-    email = request.form['SUemail']
-    password = request.form['SUpassword']
-
-    return render_template('index.html', email = email, password = password)
-
-
-
-
-
+    return render_template("index")
